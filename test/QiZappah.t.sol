00011ccda9eba786687a5bf4c03c311beb208547
Mark Aiken <2439803+royalaid@users.noreply.github.com>
2022-12-07 20:01:00 -0800

    Add simple zap in and zap out test

--- test/QiZappah.t.sol
+++ test/QiZappah.t.sol
@@ -6,29 +6,45 @@ import "forge-std/console.sol";
 import "./FakeERC20.sol";
 import "./ERC20TokenFaker.sol";
 import "../src/QiZappah.sol";
+import "openzeppelin-contracts/interfaces/IERC721Receiver.sol";
 
-contract ContractTest is Test {
+contract ContractTest is Test, ERC20TokenFaker, IERC721Receiver {
     QiZappah qiZappah;
     ICrossChainStablecoin vault;
-    ERC20TokenFaker faker;
+    ERC20 weth;
 
     function setUp() public {
+        weth = ERC20(0x4200000000000000000000000000000000000006);
         qiZappah = new QiZappah();
         vault = ICrossChainStablecoin(0xD13Ed4879DCF81C181DA82C46F4D0689b0734F23);
-        FakeERC20 fakeToken = faker.fakeOutERC20(address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2));
     }
 
-    function testExample() public {
-        /*
-        WETH ->
-        Call deposit on yearn vault @ 0xA628c54C850ff1077b5C954491D19EccE7e321fF
-        Call enter on PerfToken @ 0x5A6325c3E3c88Dbcd52a8d55a31b342d09fa7982
-        Call depositCollateral @ 0xD13Ed4879DCF81C181DA82C46F4D0689b0734F23
-        */
+    function testZapInZapOut() public {
+       address YVWETH = 0xA628c54C850ff1077b5C954491D19EccE7e321fF;
+       address PERF = 0x5A6325c3E3c88Dbcd52a8d55a31b342d09fa7982;
        
-        qiZappah.addChainToWhiteList(0x4200000000000000000000000000000000000006, 0xA628c54C850ff1077b5C954491D19EccE7e321fF,
-                                     0x5A6325c3E3c88Dbcd52a8d55a31b342d09fa7982, 0xD13Ed4879DCF81C181DA82C46F4D0689b0734F23);
+        qiZappah.addChainToWhiteList(address(weth), YVWETH, PERF, address(vault));
         uint vaultId = vault.createVault();
         console.log("%s:%s", "vaultId", vaultId);
+
+        fakeOutERC20(address(weth))._setBalance(address(this), 10e18);
+        assertEq(weth.balanceOf(address(this)), 10e18);
+
+        vault.approve(address(qiZappah), vaultId);
+        weth.approve(address(qiZappah), 10e18);
+        qiZappah.beefyZapToVault(10e18, vaultId, address(weth), YVWETH, PERF, address(vault));
+        assertEq(weth.balanceOf(address(this)), 0);
+        uint collateral = vault.vaultCollateral(vaultId);
+        console.log("%s:%s", "collateral", collateral);
+
+        vault.approve(address(qiZappah), vaultId);
+        qiZappah.beefyZapFromVault(collateral, vaultId, address(weth), YVWETH, PERF, address(vault));
+        collateral = vault.vaultCollateral(vaultId);
+        console.log("%s:%s", "collateral", collateral);
+        console.log("%s:%s", "weth", weth.balanceOf(address(this)));
+    }
+
+    function onERC721Received(address operator, address from, uint256 tokenId, bytes memory data) public pure returns (bytes4) {
+        return this.onERC721Received.selector;
     }
 }
